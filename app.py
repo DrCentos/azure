47b5120ec5472a21f306e1f0ac1dd972aa0e88ec
Nicolas VALLOT <nvallot@groupeastek.fr>
2023-06-26 11:20:14 +0200

    ok

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
 from flask import Flask, render_template, request,jsonify, url_for
-from google.cloud import storage
 import base64
-from google.cloud import aiplatform
-import google.cloud.storage
-from google.cloud import storage
-from google.cloud import aiplatform
-from os import path, getenv, makedirs, listdir
 import threading
-from google.cloud import tasks_v2
-from google.protobuf import timestamp_pb2
-from google.api_core.exceptions import NotFound
-from datetime import timedelta
 import json
 import time
-from botocore.exceptions import ClientError
+
+import sys
+from os import path
+
+current_folder = path.dirname(path.abspath(__file__))
+import subprocess
+subprocess.check_call([sys.executable, "-m", "pip", "install", "azure-ai-ml", "azure-identity","-t", current_folder])
+
+import os
+# Authentication package
+from azure.identity import DefaultAzureCredential
+from azure.ai.ml.entities import AmlCompute
+from azure.ai.ml.entities import Environment
+from azure.ai.ml import MLClient
 
 app = Flask(__name__, template_folder='template')
-s3_bucket = "nvabucket"
-
-boto_session = boto3.Session(region_name="eu-west-3")
-sagemaker_session = sagemaker.Session(boto_session=boto_session)
-endpoint_name = 'nvaendpoint'
-model_name="nvamodel"
-instance_type = 'ml.m5.xlarge'
-instance_count = 1
-session = boto3.Session()
-region = 'eu-west-3'
-sagemaker_client = session.client('sagemaker',region_name=region)
-file_name = 'job.txt'
-
+credential = DefaultAzureCredential()
 #retourne True si le model existe sinon False
 def test_model_exist(model_name):
     # Configuration de l'accès à AWS
@@ -210,65 +201,91 @@ def delete():
     return render_template('deploy.html', result=res)
 @app.route("/trainfunc", methods=["POST"])
 def trainfunc():
+    # Get a handle to the workspace
+    ml_client = MLClient(
+        credential=credential,
+        subscription_id="d9414828-c496-44ab-b6f9-e903bce40957",
+        resource_group_name="mlops",
+        workspace_name="nvml",
+    )
+    registered_model_name = 'credit_defaults_model'
 
-    #supprimer les fichier d'entrainement de l'ancien model stocké dans la bucket
-    delete_model_file_from_s3(s3_bucket,'output')
+    dependencies_dir = "./dependencies"
+    os.makedirs(dependencies_dir, exist_ok=True)
+
+    custom_env_name = "aml-scikit-learn"
+
+    # Name assigned to the compute cluster
+    cpu_compute_target = "cpu-cluster"
+
+    try:
+        # let's see if the compute target already exists
+        cpu_cluster = ml_client.compute.get(cpu_compute_target)
+        print(
+            f"You already have a cluster named {cpu_compute_target}, we'll reuse it as is."
+        )
+
+    except Exception:
+        print("Creating a new cpu compute target...")
+
+        # Let's create the Azure ML compute object with the intended parameters
+        cpu_cluster = AmlCompute(
+            name=cpu_compute_target,
+            # Azure ML Compute is the on-demand VM service
+            type="amlcompute",
+            # VM Family
+            size="STANDARD_DS2_V2",
+            # Minimum running nodes when there is no job running
+            min_instances=0,
+            # Nodes in cluster
+            max_instances=4,
+            # How many seconds will the node running after the job termination
+            idle_time_before_scale_down=180,
+            # Dedicated or LowPriority. The latter is cheaper but there is a chance of job termination
+            tier="Dedicated",
+        )
+        print(
+            f"AMLCompute with name {cpu_cluster.name} will be created, with compute size {cpu_cluster.size}"
+        )
+        # Now, we pass the object to MLClient's create_or_update method
+        cpu_cluster = ml_client.compute.begin_create_or_update(cpu_cluster)
 
 
 
-    estimator = sagemaker.estimator.Estimator(
-        image_uri='778331702232.dkr.ecr.eu-west-3.amazonaws.com/nv-train-sagemaker:latest',
-        role='arn:aws:iam::778331702232:role/nvsagemaker',
-        instance_count=instance_count,
-        instance_type=instance_type,
-        output_path='s3://nvabucket/output',
-        sagemaker_session=sagemaker_session,
-        max_run=24 * 60 * 60
+    # TODO WAIT ICI
+
+    pipeline_job_env = Environment(
+        name=custom_env_name,
+        description="Custom environment for Credit Card Defaults pipeline",
+        tags={"scikit-learn": "0.24.2"},
+        conda_file=os.path.join(dependencies_dir, "conda.yml"),
+        image="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04:latest",
+    )
+    pipeline_job_env = ml_client.environments.create_or_update(pipeline_job_env)
+
+    print(
+        f"Environment with name {pipeline_job_env.name} is registered to workspace, the environment version is {pipeline_job_env.version}"
     )
 
-    timestamp = str(int(time.time()))  # Obtenir le timestamp actuel
-    nom_nv = "nv" + timestamp
 
-    estimator.fit(job_name=nom_nv)
+    train_src_dir = "./src"
+    os.makedirs(train_src_dir, exist_ok=True)
 
-    s3 = boto3.client('s3')
-    s3.put_object(Body=nom_nv, Bucket=s3_bucket, Key=file_name)
-    print(nom_nv)
+    from azure.ai.ml import command
+    from azure.ai.ml import Input
 
-
-
-    #supprimer l'ancien endpoint s'il existe
-    delete_endpoint_from_sagemaker(endpoint_name)
-
-    #supprimer l'ancien model s'il existe
-    delete_sagemaker_model(model_name)
-    modeldata = "s3://nvabucket/output/" + nom_nv + "/output/model.tar.gz"
-    print("modeldata :", modeldata)
-    # Create the model
-    model = sagemaker.Model(
-        image_uri='778331702232.dkr.ecr.eu-west-3.amazonaws.com/nv-pred-sagemaker:latest',
-        model_data=modeldata,
-        role='arn:aws:iam::778331702232:role/nvsagemaker',
-        sagemaker_session=sagemaker_session,
-        name='nvamodel'
+    job = command(
+        code="./src/",  # location of source code
+        command="python main.py",
+        environment="aml-scikit-learn@latest",
+        compute=cpu_compute_target,
+        experiment_name="train_model_credit_default_prediction",
+        display_name="credit_default_prediction",
     )
 
-    # Deploy the model
-    predictor = model.deploy(
-        initial_instance_count=instance_count,
-        instance_type=instance_type,
-        endpoint_name=endpoint_name
-    )
-
-    # Get the endpoint
-    predictor = sagemaker.predictor.Predictor(
-        endpoint_name=endpoint_name,
-        sagemaker_session=sagemaker_session
-    )
-
+    ml_client.create_or_update(job)
     print("end")
-    #return render_template('retrain.html', result="terminé")
-#@app.route("/retrainfunc", methods=["POST"])
+
 def retrainfunc():
 
     timestamp = str(int(time.time()))  # Obtenir le timestamp actuel
